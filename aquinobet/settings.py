80b83863c1caf293b46a1bbce6f57edda46faf49
Thiago <thiagoaquino.df@gmail.com>
2024-05-05 20:57:21 -0300

    atualizacao-1.3
    novas implementações com gráfico

--- aquinobet/settings.py
+++ aquinobet/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'aquinobet.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,6 +122,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
